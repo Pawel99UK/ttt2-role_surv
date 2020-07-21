a0fe8933e6c64342c79afe815889b6a2cba74a6d
Pawel99UK <68613257+Pawel99UK@users.noreply.github.com>
2020-07-21 20:50:42 +0100

    Update and rename survivalist.lua to troller.lua

---
+++ lua/lang/english/troller.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("english")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[TROLLER.name] = "Troller"
+L["info_popup_" ..TROLLER.name] = [[You are a Troller!
+Try to survive and protect your mates if possible!]]
+L["body_found_" .. TROLLER.abbr] = "They were a Troller."
+L["search_role_" .. TROLLER.abbr] = "This person was a Troller!"
+L["target_" .. TROLLER.name] = "Troller"
+L["ttt2_desc_" .. TROLLER.name] = [[The Troll is a better innocent, because he is able to use his own ([C]) shop.
+Try to protect all innocents!]]

53d2d09b7e2b7394e2aea06d5f6c12d43e316ede
Pawel99UK <68613257+Pawel99UK@users.noreply.github.com>
2020-07-22 17:02:04 +0100

    Update shared.lua

--- lua/terrortown/entities/roles/survivalist/shared.lua
+++ lua/terrortown/entities/roles/survivalist/shared.lua
@@ -8,24 +8,26 @@ function ROLE:PreInitialize()
 	self.color = Color(80, 140, 90, 255)
 
 	self.abbr = "surv" -- abbreviation
-	self.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
+	self.surviveBonus = 1 -- bonus multiplier for every survive while another player was killed
 	self.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
 	self.scoreTeamKillsMultiplier = -8 -- multiplier for teamkill
 	self.unknownTeam = true -- player don't know their teammates
 
-	self.defaultTeam = TEAM_INNOCENT -- the team name: roles with same team name are working together
+	self.defaultTeam = TEAM_Murderer -- the team name: roles with same team name are working together
 	self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
 
 	self.conVarData = {
-		pct = 0.15, -- necessary: percentage of getting this role selected (per player)
+		pct = 0.35, -- necessary: percentage of getting this role selected (per player)
 		maximum = 2, -- maximum amount of roles in a round
-		minPlayers = 7, -- minimum amount of players until this role is able to get selected
-		credits = 1, -- the starting credits of a specific role
+		minPlayers = 6, -- minimum amount of players until this role is able to get selected
+		credits = 3, -- the starting credits of a specific role
 		togglable = true, -- option to toggle a role for a client if possible (F1 menu)
 		shopFallback = SHOP_FALLBACK_DETECTIVE
 	}
 end
 
 function ROLE:Initialize()
-	roles.SetBaseRole(self, ROLE_INNOCENT)
+	roles.SetBaseRole(self, ROLE_Murderer)
+end
+ply:GiveEquipmentWeapon('weapon_ttt_c4')
 end
